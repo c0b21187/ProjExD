ca5e14f860bbd790aade83daa71e94f2c7522c7a
c0b21187 <c0b21187d1@edu.teu.ac.jp>
2022-06-28 15:25:12 +0900

    練習10

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+import maze_maker as mm
+
 
 def key_down(event):
     global key
     key = event.keysym
-
 
 
 def key_up(event):
@@ -25,13 +26,14 @@ def main_proc():
     root.after(100, main_proc)
 
 
-
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canvas.pack()
+    maze_bg = mm.make_maze(15,9) #1:壁/0:床を表す二次元リスト
+    mm.show_maze(canvas, maze_bg) # canvasにmaze_bgを描く
 
     tori = tk.PhotoImage(file = "fig/9.png")
     cx, cy = 300, 400
