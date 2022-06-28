2f96e4e27625d4d8ae498fb89b351e7c6ca944f6
c0b21187 <c0b21187d1@edu.teu.ac.jp>
2022-06-28 15:53:57 +0900

    練習12

--- ex03/maze.py
+++ ex03/maze.py
@@ -13,15 +13,20 @@ def key_up(event):
 
 
 def main_proc():
-    global cx, cy
+    global cx, cy, mx, my
     delta = { #キー：押されているキーkey/値：移動幅リスト[x, y]
-        ""     : [0,   0],
-        "Up"   : [0, -20],
-        "Down" : [0, +20],
-        "Left" : [-20, 0],
-        "Right": [+20, 0],
+        ""     : [0,  0],
+        "Up"   : [0, -1],
+        "Down" : [0, +1],
+        "Left" : [-1, 0],
+        "Right": [+1, 0],
     }
-    cx, cy = cx + delta[key][0], cy + delta[key][1]
+    try:
+        if (maze_bg[my + delta[key][1]][mx + delta[key][0]] == 0): #移動先が床なら
+            my, mx = my + delta[key][1], mx + delta[key][0]
+    except:
+        pass
+    cx, cy = mx * 100 + 50, my * 100 + 50
     canvas.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -32,11 +37,13 @@ if __name__ == "__main__":
 
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canvas.pack()
+
     maze_bg = mm.make_maze(15,9) #1:壁/0:床を表す二次元リスト
     mm.show_maze(canvas, maze_bg) # canvasにmaze_bgを描く
 
     tori = tk.PhotoImage(file = "fig/9.png")
-    cx, cy = 300, 400
+    mx, my =1, 1
+    cx, cy = mx * 100 + 50, my * 100 + 50
     canvas.create_image(cx, cy, image = tori, tag = "tori")
 
     key = ""
