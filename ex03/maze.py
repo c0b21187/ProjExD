e6d1d017f7a5a038d8d0aac61f97f27ba00cd42b
c0b21187 <c0b21187d1@edu.teu.ac.jp>
2022-06-28 17:11:12 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -24,6 +24,8 @@ def main_proc():
     try:
         if (maze_bg[my + delta[key][1]][mx + delta[key][0]] == 0): #移動先が床なら
             my, mx = my + delta[key][1], mx + delta[key][0]
+        elif(maze_bg[my+delta[key][11]][mx+delta[key][5]] == 0): #移動先がゴールなら
+            tk.showinfo("ゴール","ゴール！！！！") #メッセージ表示
     except:
         pass
     cx, cy = mx * 100 + 50, my * 100 + 50
@@ -35,16 +37,22 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
-    canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+    canvas = tk.Canvas(root, width = 1300, height = 700, bg = "black")
     canvas.pack()
 
-    maze_bg = mm.make_maze(15,9) #1:壁/0:床を表す二次元リスト
+    maze_bg = mm.make_maze(13,7) #1:壁/0:床を表す二次元リスト
     mm.show_maze(canvas, maze_bg) # canvasにmaze_bgを描く
 
     tori = tk.PhotoImage(file = "fig/9.png")
+    box = tk.PhotoImage(file = "fig/kaizoku_takara1.png")
+    start = tk.PhotoImage(file = "fig/0218000009_1.png")
     mx, my =1, 1
     cx, cy = mx * 100 + 50, my * 100 + 50
+    sx, sy = 150,150
+    bx, by = 1150, 550
     canvas.create_image(cx, cy, image = tori, tag = "tori")
+    canvas.create_image(sx, sy, image = start, tag = "start")
+    canvas.create_image(bx, by, image = box, tag = "box")
 
     key = ""
 
