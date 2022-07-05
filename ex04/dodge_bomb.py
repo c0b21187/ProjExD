991067d8918b30f8c3fe607e2b9b9aee8488b969
c0b21187 <c0b21187d1@edu.teu.ac.jp>
2022-07-05 15:02:38 +0900

    練習6

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -26,6 +26,7 @@ def main():
     bmimg_rct = bmimg_sfc.get_rect()
     bmimg_rct.centerx = random.randint(0, screen_rct.width)
     bmimg_rct.centery = random.randint(0, screen_rct.height)
+    vx, vy = +1 ,+1 # 練習6
 
 
     while True:
@@ -42,6 +43,9 @@ def main():
         if key_states[pg.K_LEFT]  == True:kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True:kkimg_rct.centerx += 1
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
+
+        #練習6
+        bmimg_rct.move_ip(vx, vy)
 
         #練習5
         screen_sfc.blit(bmimg_sfc, bmimg_rct)
