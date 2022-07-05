431f76204caf7b578415311c3f4b6ab40897c850
c0b21187 <c0b21187d1@edu.teu.ac.jp>
2022-07-05 14:40:45 +0900

    練習4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,6 +3,8 @@ import sys
 
 def main():
     clock = pg.time.Clock()
+
+    # 練習1
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900)) # Surface
     screen_rct = screen_sfc.get_rect()
@@ -10,12 +12,26 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
+    # 練習3
+    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900, 400
+
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
         # 練習2
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        # 練習4
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP]    == True:kkimg_rct.centery -= 1
+        if key_states[pg.K_DOWN]  == True:kkimg_rct.centery += 1
+        if key_states[pg.K_LEFT]  == True:kkimg_rct.centerx -= 1
+        if key_states[pg.K_RIGHT] == True:kkimg_rct.centerx += 1
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
         
         pg.display.update()
         clock.tick(1000)
